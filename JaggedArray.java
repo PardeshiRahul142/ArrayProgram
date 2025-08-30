02be8cd0e2d10b921c8c656c447cb8c97352bf62
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-08-30 10:17:18 +0530

    Add files via upload

---
+++ JaggedArray.java
@@ -0,0 +1,35 @@
+package Arr.gth;
+import java.util.*;
+public class JaggedArray {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a[][]=new int[3][];
+		   a[0]=new int[2];
+		   a[1]=new int[5];
+		   a[2]=new int[3];
+		   
+		   System.out.println("Enter the element :");
+		   for(int i=0;i<a.length;i++)
+		   {
+			   for(int j=0;j<a[i].length;j++)
+			   {
+				   a[i][j]=sc.nextInt();
+			   }
+		   }
+		   System.out.println("Array is : ");
+		   for(int i=0;i<a.length;i++)
+		   {
+			   for(int j=0;j<a[i].length;j++)
+			   {
+				   System.out.print(" "+a[i][j]);
+			   }
+			   System.out.println();
+		   }
+			   
+		   
+		   
+
+	}
+
+}

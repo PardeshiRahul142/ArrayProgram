d41cfe9699088bd5cc7827ba02f0e91adedf6bab
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-11-08 11:06:52 +0530

    constructor in abstract class

---
+++ Ad_ConsAPP.java
@@ -0,0 +1,38 @@
+abstract class Vehicle
+{ abstract void engine();
+  abstract void color();
+  abstract void feature();
+
+  Vehicle()
+  { System.out.println("I am constructor of abstract class");
+  }
+
+}
+
+class VADP extends Vehicle
+{
+   void engine(){ }
+   void color(){ }
+   void feature(){ }
+}
+class Bike extends VADP
+{ 
+  void engine() 
+  { System.out.println("Engine is start ...");
+  }
+}
+class Car extends VADP
+{
+    void color()
+    { System.out.println("Car color is orange...");
+    }
+}
+
+public class Ad_ConsAPP
+{ public static void main(String args[])
+  {  Vehicle v=new Bike();
+             v.engine();
+             v=new Car();
+             v.color();
+  }
+}

6e8b08cc43165cc621b6315d08878d5454382c9d
RahulPardeshi <rahulpardeshi949@gmail.com>
2025-12-17 11:04:23 +0530

    This is pattern

--- TP.java
+++ TP.java
@@ -1,25 +1,38 @@
-//
+import java.util.*;
 public class TP
 {
- public static void main(String args[])
-  {
-   for(int i=1;i<=5;i++)
-    {
-	  for(int j=1;j<=5;j++)
-	  {
-	    if(j==1 ||i==5)
-		{
-		System.out.print("* ");
-		}
-		else if(j==i)
-		{
-		System.out.print("* ");
-		}
-		else{
-			System.out.print("  ");
-		}
-		}
-		System.out.println();
-	}
+   public static void main(String args[])
+  {  
+     int no=1;
+     char ch='A';
+     for(int i=1;i<=5;i++)
+     {  ch='B';
+       for(int j=1;j<=9;j++)
+       {
+           if(i<5 && ( j==6-i || j==4+i))
+           {
+             if(i%2==0)
+             {
+               System.out.print(ch);
+                 ch++;
+             }
+            else
+             {
+               System.out.print(no);
+                no++;
+             }
+           }
+          else if(i==5 && (j%2!=0))
+           {
+            System.out.print(no);
+              no++;
+           }
+          else
+           {
+              System.out.print("-");
+           }
+        }
+      System.out.println();
+     }
   }
 }

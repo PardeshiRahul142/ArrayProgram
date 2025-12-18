dc12cb5b28036149620cbcbb9543953e55d86723
RahulPardeshi <rahulpardeshi949@gmail.com>
2025-12-18 17:44:51 +0530

    THis is wait example

---
+++ WSMAPP.java
@@ -0,0 +1,58 @@
+import java.util.*;
+class Table
+{
+   public synchronized void showTable(int x)
+   {
+      try
+      {
+            for(int i=1;i<=10;i++)
+            { 
+               System.out.println(i+" "+x+" "+(i*x));
+               if(i==5)
+               { 
+                 System.out.println("I am in waiting state");
+                 wait(5000);
+               }
+              Thread.sleep(1000);
+    
+            }
+      }
+      catch(InterruptedException e)
+      {
+         System.out.println(e);
+      }
+   }
+}
+class Two extends Thread
+{
+   Table t;
+   Two(Table t){this.t=t;}
+   
+    public void run()
+    {
+     t.showTable(3);
+    }
+}
+class Three extends Thread
+{
+  Table ta;
+  Three(Table ta){ this.ta=ta;}
+
+   public void run()
+   {
+     ta.showTable(3);
+   }
+}
+public class WSMAPP
+{
+   public static void main(String args[])
+   {
+      Table t=new Table();
+ 
+     Two to=new Two(t);
+        to.start();
+
+     Three th=new Three(t);
+       th.start();
+   }
+}

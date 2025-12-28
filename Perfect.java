54f91a034374fe8770dab5a32708dddc09736e28
RahulPardeshi <rahulpardeshi949@gmail.com>
2025-12-28 16:03:37 +0530

    this

---
+++ Perfect.java
@@ -0,0 +1,25 @@
+//Strong numbet
+
+public class Perfect
+{
+  public static void main(String args[])
+  { 
+ 
+    for(int j=1;j<500;j++)
+    {
+     int sum=0;
+     for(int i=1;i<j;i++)
+     {
+         if(j % i == 0)
+         {
+            sum+=i;
+         }
+     }
+    if(sum==j)
+    { System.out.println(j);
+    }
+    }
+ 
+    
+  }
+}

0b1fad122848bd9545f0c021d1f8b473b79f1222
RahulPardeshi <rahulpardeshi949@gmail.com>
2026-01-06 21:44:38 +0530

    This comparable example

--- CAPP.java
+++ CAPP.java
@@ -1,13 +1,48 @@
 import java.util.*;
-public class CAPP
-{  public static void main(String args[])
+class Student implements Comparable<Student>
+{
+   private int id;
+   private String name;
+   private int marks;
+ 
+  public Student(int id , String name , int marks)
    { 
-       int a=100;
-       long b=a;
-   
-       long c=200;
-       int d=(int)c;
-       System.out.println(" This is explicit conversion ( int to long )d is :"+d);
-       System.out.println(" this is implicit conversion ( long to int ) b is : "+b);
+    this.id=id;
+    this.name=name;
+    this.marks=marks;
    }
+
+    public int getId(){return id;}
+    public String getName(){ return name;}
+    public int getMarks(){ return marks;}
+
+    public int compareTo(Student st)
+    {
+     
+      if(this.id< st.getId())
+      { return 1;
+      }
+     else if(this.id>st.getId())
+     {  return -1;
+     }
+    else
+     { return 0;
+     }
+  }
+}
+
+public class CAPP
+{ public static void main(String args[])
+  {
+     List<Student> li=new ArrayList<>();
+     li.add(new Student(1,"Rahul",90));
+     li.add(new Student(9,"yuv",90));
+     li.add(new Student(3,"abv",90));
+
+    Collections.sort(li);
+   for(Student s:li)
+   { 
+     System.out.println(s.getId()+"  "+ s.getName()+"  "+s.getMarks());
+  }
+  }
 }
